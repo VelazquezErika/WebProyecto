c852ff3d7e40e03d31d339ea189538c3b06541d8
jasso322 <l23211914@tectijuana.edu.mx>
2026-04-23 01:51:10 +0000

    Merge de las reseñas y peliculas favoritas con el buscador funcional

--- movies/urls.py
+++ movies/urls.py
@@ -1,17 +1,26 @@
 from django.urls import path
-from . import views 
+from . import views  # Usaremos 'views.nombre' para que sea más claro y organizado
 
 urlpatterns = [
-    path('all/', all_movies),
-    path('<int:movie_id>/', movie),
-    path('saludo/<int:veces>/', saludo),
-    path('movie_review/add/<int:movie_id>/', add_review),
-    path('movie_Mas/add/<int:movie_id>/', add_Mas),
-    path('movie_prueba/add<int:movie_id>', add_prueba),
-    path('movie_reviews/<int:movie_id>/', movie_reviews, name='movie_reviews'),
-    path('search/', search_movies, name='search_movies'),#url para la busqueda de peliculas
+    # Rutas Principales
+    path('', views.index, name='index'),
+    path('all/', views.all_movies, name='all_movies'),
+    path('search/', views.search_movies, name='search_movies'),
+    path('<int:movie_id>/', views.movie, name='movie'),
+    
+    # Colecciones y Mis Películas
     path('mis-peliculas/', views.mis_peliculas, name='mis_peliculas'),
     path('collections/', views.collections, name='collections'),
+    
+    # Reseñas
+    path('movie_review/add/<int:movie_id>/', views.add_review, name='add_review'),
+    path('movie_reviews/<int:movie_id>/', views.movie_reviews, name='movie_reviews'),
+    
+    # Gestión de "Mas" (Añadir/Quitar)
+    path('movie_Mas/add/<int:movie_id>/', views.add_Mas, name='add_Mas'),
     path('movie_Mas/remove/<int:movie_id>/', views.remove_Mas, name='remove_Mas'),
-    path('', index)
+    
+    # Pruebas y Otros
+    path('movie_prueba/add/<int:movie_id>/', views.add_prueba, name='add_prueba'),
+    path('saludo/<int:veces>/', views.saludo, name='saludo'),
 ]
